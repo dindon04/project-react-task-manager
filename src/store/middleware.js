f7ad44bb05382bf244b9e03129a7eb337a9bb14d
dindon04 <150649289+dindon04@users.noreply.github.com>
2025-01-15 19:31:24 +0400

    reducers for tasks and filters

---
+++ src/store/middleware.js
@@ -0,0 +1,8 @@
+export const localStorageMiddleware = (store) => (next) => (action) => {
+    const result = next(action);
+    console.log("Current state:", store.getState());
+    localStorage.setItem("reduxState", JSON.stringify(store.getState()));
+    console.log("Redux state saved to localStorage:", store.getState());
+    return result;
+  };
+  
